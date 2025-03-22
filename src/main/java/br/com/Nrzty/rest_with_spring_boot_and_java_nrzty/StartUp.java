4a94112d012bb95cd08a834519e30afabcfa71da
Nrzty <silviosantana2000@outlook.com>
2025-03-21 21:59:48 -0300

    Adicionei os arquivos do Spring Boot pegos pelo Spring Initializer

---
+++ src/main/java/br/com/Nrzty/rest_with_spring_boot_and_java_nrzty/StartUp.java
@@ -0,0 +1,11 @@
+package br.com.Nrzty.rest_with_spring_boot_and_java_nrzty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+    public static void main(String[] args) {
+        SpringApplication.run(StartUp.class, args);
+    }
+}
---
+++ src/test/java/br/com/Nrzty/rest_with_spring_boot_and_java_nrzty/RestWithSpringBootAndJavaNrztyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Nrzty.rest_with_spring_boot_and_java_nrzty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootAndJavaNrztyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
